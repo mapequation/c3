10e125c70720cc1036d4d59f765a7a8bb8eb8151
danieledler <danieledler@users.noreply.github.com>
2021-11-05 12:01:30 +0000

    deploy: ac6025dfa2641ee2f46df638cf086ddab2bb0367

--- _next/static/FP9ETIuvNy5XhQNE38yte/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ae51ba48-e64140b3aef6860d.js","static/chunks/841-1eb49d61a2ddcf5a.js","static/chunks/pages/index-e4d0fb411f3a9e3e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/SBOyjFmUnKjv_-qjroicE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ae51ba48-e64140b3aef6860d.js","static/chunks/841-1eb49d61a2ddcf5a.js","static/chunks/pages/index-3e309787efcc65c5.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
