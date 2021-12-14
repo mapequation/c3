d3c7c285414be0365a11be4df36e8120de7777e9
danieledler <danieledler@users.noreply.github.com>
2021-12-14 17:40:59 +0000

    deploy: 11b07771f846282376622e28a2d22152bd77f2ea

--- _next/static/Cxvh7wUmuGOR2hpOA1qct/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ae51ba48-e64140b3aef6860d.js","static/chunks/569-a7c9d81dd8a682ff.js","static/chunks/pages/index-5a35c26349480c0a.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
